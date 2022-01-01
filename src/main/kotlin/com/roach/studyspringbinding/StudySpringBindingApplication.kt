7aee17614f3a38dc483a0e63384c7cb6818611fb
roach <tmdgusya@naver.com>
2022-01-01 09:49:23 +0900

    init

---
+++ src/main/kotlin/com/roach/studyspringbinding/StudySpringBindingApplication.kt
@@ -0,0 +1,11 @@
+package com.roach.studyspringbinding
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudySpringBindingApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudySpringBindingApplication>(*args)
+}
---
+++ src/test/kotlin/com/roach/studyspringbinding/StudySpringBindingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.roach.studyspringbinding
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudySpringBindingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
